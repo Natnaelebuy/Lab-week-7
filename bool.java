4ffb6952b7dad6efd6229e5bc40c31c3e0af38eb
Natnael <natnaelebuy@gmail.com>
2021-10-12 19:03:16 -0700

    update

---
+++ bool.java
@@ -0,0 +1,31 @@
+public class bool{
+    public static void main(String[] args){
+      
+        // Using primitive boolean variable:
+      boolean Isateacher = true;
+      if(Isateacher)
+        System.out.println("Is a teacher = " + Isateacher);
+      
+      // Using java.lang.Boolean:
+      Boolean Isover65yearsold = false;
+      if(Isover65yearsold .toString().equals("false"))
+        System.out.println("Is over 65 years old  = " + Isover65yearsold );
+     
+        // Using java.lang.Boolean:
+    Boolean Hasamedicalcondition = false;
+    if(Hasamedicalcondition .toString().equals("false"))
+      System.out.println("Has a medical condition = " + Hasamedicalcondition );
+
+      // Using java.lang.Boolean:
+      Boolean Canbegivenavaccination = true;
+      if(Canbegivenavaccination.toString().equals("true"))
+        System.out.println("Can be given a vaccination  = " + Canbegivenavaccination );
+
+
+     
+    
+    
+    
+    
+    }
+  }
