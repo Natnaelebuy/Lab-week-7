75e87c47baf5e749128de1ebbedc7ec2317c9979
Natnael <natnaelebuy@gmail.com>
2021-10-13 22:15:51 -0700

    change

---
+++ JSONArray.java
@@ -0,0 +1,13 @@
+import org.json.simple.JSONObject;
+
+public class JSONArray {
+
+    public Integer size() {
+        return null;
+    }
+
+    public JSONObject get(Integer i) {
+        return null;
+    }
+
+}
